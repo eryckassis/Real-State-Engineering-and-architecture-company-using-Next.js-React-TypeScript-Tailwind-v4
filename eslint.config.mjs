e5c37699fde5d3e8571be9794a480bb46ac21275
Eryck Assis <eng.assis.dev@gmail.com>
2025-12-07 20:14:42 -0300

    feat: Implement Menu Button and Navigation Components
    - Added MenuBtn component with styles for a toggle button.
    - Created Nav component with animated menu transitions and links.
    - Developed Spotlight component for showcasing images with scroll-triggered animations.
    - Introduced TopBar component with scroll behavior and navigation.
    - Added useViewTransition hook for smooth page transitions.
    - Configured TypeScript settings in tsconfig.json for improved type safety.

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = (dirname = dirname(__filename));
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // typescript
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+
+      // React
+      "react/self-closing-comp": "error",
+      "react/jsx-curly-brace-presence": [
+        "error",
+        { props: "never", children: "never" },
+      ],
+
+      // Import
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc" },
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
